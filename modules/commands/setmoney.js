acb727befd5ba0f6fed014a007f4ce9c22ea0f31
Quang Sáng <46513828+sunii-g@users.noreply.github.com>
2021-05-20 04:43:43 +0700

    Update setmoney.js

--- modules/commands/setmoney.js
+++ modules/commands/setmoney.js
@@ -1,27 +1,43 @@
 module.exports.config = {
-	name: "setmoney",
+	name: "money",
 	version: "0.0.1",
 	hasPermssion: 2,
 	credits: "Sunii",
 	description: "Thay đổi tiền tệ ",
 	commandCategory: "economy",
-	usages: "setmoney @tag 5000",
+	usages: "money @tag 5000",
 	cooldowns: 5,
 };
 
-module.exports.run = function({ api, event, args, Currencies }) {
-           let { threadID, senderID, messageID } = event;
-	        var mention = Object.keys(event.mentions)[0];
-			var content = args.join(" ");
-			var sender = args[0];
-			var moneySet = args[args.length -1];
-			if (isNaN(moneySet)) return api.sendMessage("Số tiền bạn nhập không hợp lệ", threadID, messageID);
-			if (!mention && sender == 'me') return api.sendMessage(`Bạn đã sửa tiền cho bản thân thành ${moneySet.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} coin`, threadID, () => Currencies.increaseMoney(senderID, parseInt(moneySet)), messageID);
-			return api.sendMessage({
-				body: `Bạn đã sửa tiền cho ${event.mentions[mention].replace("@", "")} thành ${moneySet.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} coin`,
-				mentions: [{
-					tag: event.mentions[mention].replace("@", ""),
-					id: mention
-				}]
-			}, threadID, () => Currencies.increaseMoney(mention, parseInt(moneySet)), messageID);
+module.exports.run = async function({ api, event, args, Currencies }) {
+  let { threadID, senderID, messageID } = event;
+	const mentions = Object.keys(event.mentions)[0];
+	const content = args.join(" ");
+  const sender = args[0];
+  const moneySet = args[args.length -1];
+  const moneyMentions = args[args.length -1];
+  if (isNaN(moneySet)) return api.sendMessage("Số tiền bạn nhập không hợp lệ", threadID, messageID);
+  switch(args[0]) {
+    case 'add':
+      if (!mentions && sender) return api.sendMessage(`Bạn đã nạp ${moneySet.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} coin vào ví`, event.threadID,() => Currencies.increaseMoney(senderID, parseInt(moneyMentions)), messageID)
+      return api.sendMessage({
+        body: `Bạn đã nạp cho ${event.mentions[mentions].replace("@", "")} ${moneyMentions.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} vào ví.`, 
+        mentions: [{tag: event.mentions[mentions].replace("@", ""),id: mentions}]},
+        threadID, () => Currencies.increaseMoney(mentions, parseInt(moneyMentions)), messageID)
+    break;
+    case 'move':
+      if (!mentions && sender) return api.sendMessage(`Bạn đã trừ ${moneySet.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} coin khỏi ví`, event.threadID,() => Currencies.decreaseMoney(senderID, parseInt(moneyMentions)), messageID)
+      return api.sendMessage({
+        body: `Bạn đã trừ ${moneyMentions.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} coin từ ví của ${event.mentions[mentions].replace("@", "")}.`, 
+        mentions: [{tag: event.mentions[mentions].replace("@", ""),id: mentions}]},
+        threadID, () => Currencies.decreaseMoney(mentions, parseInt(moneyMentions)), messageID)
+    break;
+    case 'set':
+      if (!mentions && sender) return api.sendMessage(`Bạn đã thay đổi số dư thành ${moneySet.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} coin`, threadID, () => Currencies.setData(senderID,{money: parseInt(moneySet)}), messageID);
+		  return api.sendMessage({
+        body: `Bạn đã thay đổi số dư cho ${event.mentions[mentions].replace("@", "")} thành ${moneySet.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} coin`,
+        mentions: [{tag: event.mentions[mentions].replace("@", ""),id: mentions}]
+		}, threadID, () => Currencies.setData(mentions, {money: parseInt(moneySet)}), messageID);
+    break;
+  }
 }
